bcd78f83e41daa1744b55f35cee0e9e4602034d1
BearnMountain <161377261+BearnMountain@users.noreply.github.com>
2024-10-13 13:01:44 -0700

    Create setup.sh
    setup requirements for dotfiles, executeable in terminal

---
+++ Setup/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install bash ripgrep tree nvim docker fastfetch # standar
+brew install koekeishiya/formulae/yabai koekeishiya/formulae/skhd # tiling manager
+
+
+
+# for neovim, do ":checkheath" and brew install all necessary files to properly use neovim
